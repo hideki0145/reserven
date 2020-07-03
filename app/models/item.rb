8e17bc5d8c12ca717b6336d7236a7a60b83af543
Hideki Miyamoto <hideki0145@gmail.com>
2020-07-04 05:50:51 +0900

    Add: ステータス表示機能を追加する
    使用中判定メソッドを追加する。
    ステータス表示機能を追加する。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
+  has_one :status, dependent: :destroy
+
   validates :name, presence: true
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,8 @@
 class Status < ApplicationRecord
   belongs_to :item
   belongs_to :user
+
+  def use?
+    use_flg && Time.current < expires_at
+  end
 end
